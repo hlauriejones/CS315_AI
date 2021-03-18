14c1cab73b9b4e58ce62c907a72ba4867e65f731
James Lawson <jameslawson00031@gmail.com>
2021-03-18 12:25:07 -0400

    Update RNN_TextAnalysis.py
    initial progress

--- Project 4-RNN/RNN_TextAnalysis.py
+++ Project 4-RNN/RNN_TextAnalysis.py
@@ -1,7 +1,21 @@
+"""
+Authors: Laurie Jones and James Lawson
+
+
+Links:
+https://www.kite.com/python/answers/how-to-copy-columns-to-a-new-pandas-dataframe-in-python
+https://datatofish.com/count-nan-pandas-dataframe/
+https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.count.html
+https://www.kite.com/python/answers/how-to-drop-empty-rows-from-a-pandas-dataframe-in-python
+https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.reset_index.html
+https://pypi.org/project/series/
+"""
 import numpy as np 
 import pandas as pd 
 import matplotlib.pyplot as plt
 import re
+#imported series seperatly
+import series
 import nltk
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.preprocessing.text import Tokenizer
@@ -9,23 +23,50 @@ from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 
 #Read in csv file into pandas data frame
-
+data = pd.read_csv("consumer_complaints.csv")
 
 #Extract complaints and products into a new dataframe
+new_data = data[["consumer_complaint_narrative","product"]]
 
 #Check how many values are null and count them
+nullVals = new_data["consumer_complaint_narrative"].isnull()
+
+nullCounts = nullVals.sum()
+
+print("We have", str(nullCounts), "null values.")
 
 #Check how many product values we have
+productValue = new_data["product"].count()
+
+print("We have", productValue, "product values.")
 
 #Drop all empty values, reset the index, and drop them
+new_data = new_data.dropna()
+new_data = new_data.reset_index(drop = True)
+
+
+#drop the indexes 
+#idk how to do this atm ---------------------------------------
 
 #Recount product values
+total_product_values = len(new_data)
+print("We have", total_product_values, "product values.")
 
 
+#currently working on converting the text to lower case
+
+
+new_data = new_data.str.lower()
+
+
+print(new_data.head)
 
 #Create a clean text function
-
+def clean_text(new_data):
+    
     #Lower case the text
+    
+    
     
     #Compile pattern to remove all other characters
     
@@ -44,7 +85,7 @@ from nltk.corpus import stopwords
     #Join all the text by " "
     
     #Return the clean text
-    
+    pass
 
 #Apply clean text to the complaints
 
@@ -71,4 +112,3 @@ from nltk.corpus import stopwords
 
 #Print the first example of the tokenizer object to the sequences to text
 
-    
